3532b78a5f9cb0a769e26cbb1924f00d272d64a4
Nika <nvjagrdsohgjb@gmail.com>
2018-10-30 20:58:01 +0300

    first commit

---
+++ BugTracking/BugTracking/BugTracking/Classes/BaseTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracking.Classes
+{
+    public abstract class BaseTask
+    {
+        public abstract string name { get; set; }
+        public abstract int prior { get; set; }
+        public abstract int level { get; set; }
+        public abstract int time { get; set; }
+        public abstract int CalcPrior();
+        public abstract int CalcTime();
+    }
+}
---
+++ BugTracking/BugTracking/BugTracking/Classes/Tech.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracking.Classes
+{
+    class Tech : BaseTask
+    {
+        public override string name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int prior { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int level { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int time { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override int CalcPrior()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int CalcTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
